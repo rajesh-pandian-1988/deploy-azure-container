e0f20e6da4055a66c824480e05e129379875a72b
Rajesh Pandian <rajesh_pandian@outlook.com>
2023-07-16 01:50:34 +0100

    pipeline changes

---
+++ main.tf
@@ -0,0 +1,11 @@
+resource "azurerm_resource_group" "azure_contianer_rg" {
+  name     = "azure-container-rg"
+  location = var.location
+}
+
+resource "azurerm_container_registry" "terraform_docker_acr" {
+  name                = "terraformdockeracr"
+  resource_group_name = azurerm_resource_group.azure_contianer_rg.name
+  location            = azurerm_resource_group.azure_contianer_rg.location
+  sku                 = "Basic"
+}
